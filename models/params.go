140891733475c4ecc9e715a5136675666fa116dd
Kevin Suñer <kevinsunercontacto@gmail.com>
2022-03-12 17:47:16 +0100

    feat: added data structure for query parameters

---
+++ models/params.go
@@ -0,0 +1,11 @@
+package models
+
+// Params represents the data structure of the query parameters.
+type Params struct {
+	IDList string
+	Key    string
+	Token  string
+	Title  string
+	Number string
+	URL    string
+}
