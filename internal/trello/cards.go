014cc0056a79049619e96a020badc5ff0912fc02
ksrof <kevinsunercontacto@gmail.com>
2022-03-31 17:17:06 +0200

    [feat] create trello card with title and url

---
+++ internal/trello/cards.go
@@ -0,0 +1,40 @@
+package trello
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ksrof/trello-action/internal/models"
+	"github.com/ksrof/trello-action/internal/utils"
+)
+
+// CreateCard performs a POST request to api.trello.com/1/cards.
+func CreateCard(env models.Env, title, url string) error {
+	reqURL, err := utils.ParseURL("https://api.trello.com/1", "/cards")
+	if err != nil {
+		return fmt.Errorf("failed to parse url: %v", err)
+	}
+
+	req, err := http.NewRequest("POST", reqURL, nil)
+	if err != nil {
+		return fmt.Errorf("failed to build new http request: %v", err)
+	}
+
+	// Set query parameters.
+	query := req.URL.Query()
+	query.Add("idList", env.Trello.IDList)
+	query.Add("name", title)
+	query.Add("urlSource", url)
+	query.Add("key", env.Trello.Key)
+	query.Add("token", env.Trello.Token)
+
+	// Encode values into the URL.
+	req.URL.RawQuery = query.Encode()
+
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send POST request to the server: %v", err)
+	}
+
+	return nil
+}
