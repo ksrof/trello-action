c21719f7a85878d783e11b5ce47e344307d2ffd2
ksrof <kevinsunercontacto@gmail.com>
2022-04-01 18:41:21 +0200

    [feat] update trello card position with id and idList

--- internal/trello/cards.go
+++ internal/trello/cards.go
@@ -39,6 +39,35 @@ func CreateCard(env models.Env, title, url string) error {
 	return nil
 }
 
+// UpdateCard performs a PUT request to api.trello.com/1/cards/id.
+func UpdateCard(env models.Env, id, idList string) error {
+	reqURL, err := utils.ParseURL("https://api.trello.com/1", fmt.Sprintf("/cards/%s", id))
+	if err != nil {
+		return fmt.Errorf("failed to parse url: %v", err)
+	}
+
+	req, err := http.NewRequest("PUT", reqURL, nil)
+	if err != nil {
+		return fmt.Errorf("failed to build new http request: %v", err)
+	}
+
+	// Set query parameters.
+	query := req.URL.Query()
+	query.Add("idList", idList)
+	query.Add("key", env.Trello.Key)
+	query.Add("token", env.Trello.Token)
+
+	// Encode values into the URL.
+	req.URL.RawQuery = query.Encode()
+
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send PUT request to the server: %v", err)
+	}
+
+	return nil
+}
+
 // DeleteCard performs a DELETE request to api.trello.com/1/cards/id.
 func DeleteCard(env models.Env, id string) error {
 	reqURL, err := utils.ParseURL("https://api.trello.com/1", fmt.Sprintf("/cards/%s", id))
