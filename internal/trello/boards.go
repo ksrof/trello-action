3db1eeec085e60cc790898b282d7e450d1956146
ksrof <kevinsunercontacto@gmail.com>
2022-04-01 18:39:20 +0200

    [feat] get all lists from a specific board

--- internal/trello/boards.go
+++ internal/trello/boards.go
@@ -51,3 +51,45 @@ func GetCards(env models.Env) ([]map[string]interface{}, error) {
 
 	return response, nil
 }
+
+// GetLists performs a GET request to api.trello.com/1/boards/id/lists.
+// It returns the response object unmarshaled inside a map.
+func GetLists(env models.Env) ([]map[string]interface{}, error) {
+	reqURL, err := utils.ParseURL("https://api.trello.com/1", fmt.Sprintf("/boards/%s/lists", env.Trello.IDBoard))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse url: %v", err)
+	}
+
+	req, err := http.NewRequest("GET", reqURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build new http request: %v", err)
+	}
+
+	// Set query parameters.
+	query := req.URL.Query()
+	query.Add("key", env.Trello.Key)
+	query.Add("token", env.Trello.Token)
+
+	// Encode values into the URL.
+	req.URL.RawQuery = query.Encode()
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send GET request to the server: %v", err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read body response: %v", err)
+	}
+
+	// response holds all data required to represent the lists.
+	var response []map[string]interface{}
+
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal body response: %v", err)
+	}
+
+	return response, nil
+}
