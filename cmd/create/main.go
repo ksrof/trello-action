12db803fb19687db66497c50e866a4ed1b7a5e4b
Kevin Suñer <89794522+ksrof@users.noreply.github.com>
2022-06-25 11:30:39 +0200

    feat: structure cleanup, added multiple test cmds (#24)

--- cmd/create/main.go
+++ cmd/create/main.go
@@ -1,47 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"log"
+import "log"
 
-	"github.com/ksrof/trello-action/internal/configs"
-	"github.com/ksrof/trello-action/internal/github"
-	"github.com/ksrof/trello-action/internal/trello"
+const (
+	name    string = "trello-action"
+	version string = "1.0.0"
+	author  string = "ksrof"
+	env     string = "dev"
+	cmd     string = "create"
 )
 
 func main() {
-	env, err := configs.Environment()
-	if err != nil {
-		log.Fatalf("failed to load environment: %v", err)
-	}
-
-	if env.Github.Event == "issues" {
-		issue, err := github.GetIssue(*env)
-		if err != nil {
-			log.Printf("failed to get issue: %v", err)
-		}
-
-		issueTitle := fmt.Sprint(issue["title"])
-		issueURL := fmt.Sprint(issue["html_url"])
-
-		err = trello.CreateCard(*env, issueTitle, issueURL)
-		if err != nil {
-			log.Printf("failed to create card: %v", err)
-		}
-	}
-
-	if env.Github.Event == "pull_request" {
-		pull, err := github.GetPull(*env)
-		if err != nil {
-			log.Printf("failed to get pull: %v", err)
-		}
-
-		pullTitle := fmt.Sprint(pull["title"])
-		pullURL := fmt.Sprint(pull["html_url"])
-
-		err = trello.CreateCard(*env, pullTitle, pullURL)
-		if err != nil {
-			log.Printf("failed to create card: %v", err)
-		}
-	}
+	log.Printf(
+		"Name: %s\n Version: %s\n Author: %s\n Env: %s\n Command: %s\n",
+		name,
+		version,
+		author,
+		env,
+		cmd,
+	)
 }
