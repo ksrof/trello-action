6b46cd9d30d7fac31485e8dede46ae5a63365b90
Kevin Suñer <kevinsunercontacto@gmail.com>
2022-07-21 12:38:38 +0200

    feat: tried out github.Get and github.GetLabels alongside utils.WithMap

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -6,25 +6,66 @@ SPDX-License-Identifier: Apache-2.0
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
 	"github.com/ksrof/trello-action/external/github"
+	"github.com/ksrof/trello-action/utils"
 )
 
 func main() {
 	auth, err := github.NewAuth(
-		github.WithUser("johndoe"),
-		github.WithRepo("floppyunicorn"),
-		github.WithToken("token"),
+		github.WithToken("ghp_F41fR24d9Tvn3YRzC7GdOPAfhgjBzP5MLP7c"),
 	)
 	if err != nil {
 		log.Printf(
-			"failed to create a new instance of *github.auth, error: %s",
+			"failed to create a new instance of *github.auth, error: %s\n",
 			err.Error(),
 		)
 		return
 	}
 
-	fmt.Println(auth.Basic())
+	issues := new(github.IssuesResponse)
+	issue, err := issues.Get(
+		context.TODO(),
+		[]utils.Field{
+			utils.WithMap(map[string]string{
+				"username":    "ksrof",         // TODO: Use os.Getenv
+				"repository":  "trello-action", // TODO: Use os.Getenv
+				"issue_id":    "30",            // TODO: Use os.Getenv
+				"request_url": "https://api.github.com/repos/%s/%s/issues/%s",
+				"token":       auth.Basic(),
+			}),
+		})
+	if err != nil {
+		log.Printf(
+			"failed to use method github.Get(), error: %s\n",
+			err.Error(),
+		)
+		return
+	}
+
+	issueLabels, err := issues.GetLabels(
+		context.TODO(),
+		[]utils.Field{
+			utils.WithMap(map[string]string{
+				"username":    "ksrof",         // TODO: Use os.Getenv
+				"repository":  "trello-action", // TODO: Use os.Getenv
+				"issue_id":    "30",            // TODO: Use os.Getenv
+				"request_url": "https://api.github.com/repos/%s/%s/issues/%s/labels",
+				"token":       auth.Basic(),
+			}),
+		})
+	if err != nil {
+		log.Printf(
+			"failed to use method github.GetLabels(), error: %s\n",
+			err.Error(),
+		)
+		return
+	}
+
+	fmt.Println(issue)
+	fmt.Println("---------------------------")
+	fmt.Println(issueLabels)
 }
