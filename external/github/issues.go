7ba50aef93efa865745d50365db003f309677b67
Kevin Suñer <kevinsunercontacto@gmail.com>
2022-07-22 11:57:17 +0200

    wip: generated mocks and added tests for the methods

--- external/github/issues.go
+++ external/github/issues.go
@@ -1,3 +1,8 @@
+/*
+Copyright 2022 Kevin Suñer
+SPDX-License-Identifier: Apache-2.0
+*/
+
 package github
 
 import (
@@ -11,12 +16,18 @@ import (
 	"github.com/ksrof/trello-action/utils"
 )
 
+// TODO: Generate mocks for the Issues interface.
+// TODO: Test that every method works as expected.
+
+// Issues wraps methods that handle issue requests
+// of an authenticated user within the Github API.
 //go:generate mockgen -destination=mock/issues.go -package=mock . Issues
 type Issues interface {
 	Get(ctx context.Context, opts []utils.Field) (*IssuesResponse, error)
 	GetLabels(ctx context.Context, opts []utils.Field) (*IssuesResponse, error)
 }
 
+// IssueResponse represents the data returned by issue requests.
 type IssuesResponse struct {
 	Response interface{}
 	Status   string
@@ -24,6 +35,7 @@ type IssuesResponse struct {
 	Error    string
 }
 
+// Get returns a specific issue by its identifier.
 func (r *IssuesResponse) Get(ctx context.Context, opts []utils.Field) (*IssuesResponse, error) {
 	fields, err := utils.NewFieldsMapper(opts...)
 	if err != nil {
@@ -86,6 +98,7 @@ func (r *IssuesResponse) Get(ctx context.Context, opts []utils.Field) (*IssuesRe
 	}, nil
 }
 
+// GetLabels returns the labels from a specific issue by its identifier.
 func (r *IssuesResponse) GetLabels(ctx context.Context, opts []utils.Field) (*IssuesResponse, error) {
 	fields, err := utils.NewFieldsMapper(opts...)
 	if err != nil {
